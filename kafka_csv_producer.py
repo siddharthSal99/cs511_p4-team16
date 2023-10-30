b1e6cefea303471a319ce408e00ac5f40d63929a
henrywangballer <60195887+henrywangballer@users.noreply.github.com>
2023-10-30 17:14:08 -0500

    added loop for csv data

--- kafka_csv_producer.py
+++ kafka_csv_producer.py
@@ -6,23 +6,24 @@ kafka_config = {
 }
 producer = Producer(kafka_config)
 
-kafka_topic = 'json_csv_topic'
+kafka_topic = 'art_daily_jumpsup'
 
 csv_file_path = './data/art_daily_jumpsup.csv'
 
-with open(csv_file_path, 'r') as csv_file:
-    header = []
-    for line_number, line in enumerate(csv_file):
-        line_data = line.strip().split(',')
+while True:
+    with open(csv_file_path, 'r') as csv_file:
+        header = []
+        for line_number, line in enumerate(csv_file):
+            line_data = line.strip().split(',')
 
-        if line_number == 0:
-            header = [l.strip() for l in line_data]
-            continue
+            if line_number == 0:
+                header = [l.strip() for l in line_data]
+                continue
 
-        csv_json = {header[i]: line_data[i] for i in range(len(header))}
+            csv_json = {header[i]: line_data[i] for i in range(len(header))}
 
-        producer.produce(kafka_topic, value=json.dumps(csv_json).encode('utf-8'))
-        # print(f'message sent: {csv_json}')
-csv_file.close()
-producer.flush()
+            producer.produce(kafka_topic, value=json.dumps(csv_json).encode('utf-8'))
+            # print(f'message sent: {csv_json}')
+    csv_file.close()
+    producer.flush()
 
